21447412362cbf94e3907e9fec1417be6038e9e2
injectx <rogervaessen@gmail.com>
2026-05-18 23:24:52 +0200

    reate Bosch sensor entities

--- custom_components/bosch_ebike_connect/sensor.py
+++ custom_components/bosch_ebike_connect/sensor.py
@@ -4,23 +4,28 @@ from homeassistant.components.sensor import SensorEntity
 async def async_setup_entry(hass, entry, async_add_entities):
 
 	data = hass.data["bosch_ebike_connect"][entry.entry_id]
+
 	devices = data["devices"]
 
 	bike = devices["my_ebikes"][0]
 
 	sensors = [
+
 		BoschSensor(
 			"Bike Name",
 			bike["drive_unit"]["device_name"]
 		),
+
 		BoschSensor(
 			"Motor",
 			bike["drive_unit"]["product_line_name"]
 		),
+
 		BoschSensor(
 			"Display",
 			bike["buis"][0]["device_name"]
 		),
+
 		BoschSensor(
 			"Battery",
 			bike["batteries"][0]["device_name"]
