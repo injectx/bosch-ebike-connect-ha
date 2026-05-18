6136933bdc2eaf218bcc8dc9dc5adb21a22c506b
injectx <rogervaessen@gmail.com>
2026-05-18 22:51:27 +0200

    Create API class and store credentials

--- custom_components/bosch_ebike_connect/__init__.py
+++ custom_components/bosch_ebike_connect/__init__.py
@@ -2,13 +2,15 @@ DOMAIN = "bosch_ebike_connect"
 
 
 async def async_setup(hass, config):
-	return True
-
-
-async def async_setup_entry(hass, entry):
 	hass.data.setdefault(DOMAIN, {})
 	return True
 
 
+async def async_setup_entry(hass, entry):
+	hass.data[DOMAIN][entry.entry_id] = entry.data
+	return True
+
+
 async def async_unload_entry(hass, entry):
+	hass.data[DOMAIN].pop(entry.entry_id)
 	return True
---
+++ custom_components/bosch_ebike_connect/api.py
@@ -0,0 +1,8 @@
+class BoschApi:
+
+def __init__(self, email, password):
+	self.email = email
+	self.password = password
+
+async def login(self):
+	return True
