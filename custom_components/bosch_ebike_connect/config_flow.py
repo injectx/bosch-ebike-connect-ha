c24d2f5b28763811bd7238dc5f531d5215af0301
injectx <rogervaessen@gmail.com>
2026-05-18 22:44:25 +0200

    Add Bosch login form

--- custom_components/bosch_ebike_connect/config_flow.py
+++ custom_components/bosch_ebike_connect/config_flow.py
@@ -1,13 +1,30 @@
 from homeassistant import config_entries
+import voluptuous as vol
 
 DOMAIN = "bosch_ebike_connect"
+
+CONF_EMAIL = "email"
+CONF_PASSWORD = "password"
 
 
 class BoschFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 	VERSION = 1
 
 	async def async_step_user(self, user_input=None):
-		return self.async_create_entry(
-			title="Bosch eBike Connect",
-			data={}
+		if user_input is not None:
+			return self.async_create_entry(
+				title="Bosch eBike Connect",
+				data=user_input
+			)
+
+		schema = vol.Schema(
+			{
+				vol.Required(CONF_EMAIL): str,
+				vol.Required(CONF_PASSWORD): str,
+			}
+		)
+
+		return self.async_show_form(
+			step_id="user",
+			data_schema=schema
 		)
