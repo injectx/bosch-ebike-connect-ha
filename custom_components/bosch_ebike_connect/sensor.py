1337ac98770a555ae60ab4855e926556f69fdfc5
injectx <rogervaessen@gmail.com>
2026-05-18 23:21:50 +0200

    Add first Bosch sensors

--- custom_components/bosch_ebike_connect/sensor.py
+++ custom_components/bosch_ebike_connect/sensor.py
@@ -1,6 +1,37 @@
 from homeassistant.components.sensor import SensorEntity
 
 
-class BoschBikeTestSensor(SensorEntity):
-	_attr_name = "Bosch Bike Test"
-	_attr_native_value = "Connected"
+async def async_setup_entry(hass, entry, async_add_entities):
+
+	data = hass.data["bosch_ebike_connect"][entry.entry_id]
+	devices = data["devices"]
+
+	bike = devices["my_ebikes"][0]
+
+	sensors = [
+		BoschSensor(
+			"Bike Name",
+			bike["drive_unit"]["device_name"]
+		),
+		BoschSensor(
+			"Motor",
+			bike["drive_unit"]["product_line_name"]
+		),
+		BoschSensor(
+			"Display",
+			bike["buis"][0]["device_name"]
+		),
+		BoschSensor(
+			"Battery",
+			bike["batteries"][0]["device_name"]
+		)
+	]
+
+	async_add_entities(sensors)
+
+
+class BoschSensor(SensorEntity):
+
+	def __init__(self, name, value):
+		self._attr_name = f"Bosch {name}"
+		self._attr_native_value = value
